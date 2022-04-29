d42b9057ea5b0decebd0585fa699d6ab91136eaf
shanu12joshi <shanu12joshi@gmail.com>
2022-04-29 17:00:52 +0530

    Like dislike

--- lib/ImageView.dart
+++ lib/ImageView.dart
@@ -8,91 +8,90 @@ class ImageView extends StatefulWidget {
   final String imageURL;
   final String? role;
 
-  const ImageView(this.snapshot,this.imageURL, this.role);
+  const ImageView(this.snapshot, this.imageURL, this.role);
 
   @override
   State<ImageView> createState() => _ImageViewState();
 }
 
 class _ImageViewState extends State<ImageView> {
+
+  bool doesExists = false;
+
   @override
+  Future<bool> doesNameAlreadyExist(String? docId) async {
+    final QuerySnapshot result = await FirebaseFirestore.instance
+        .collection('selectedArt')
+        .where('id', isEqualTo: docId)
+        .limit(1)
+        .get();
+    final List<DocumentSnapshot> documents = result.docs;
+    return documents.length == 1;
+  }
+
   Widget build(BuildContext context) {
+    doesNameAlreadyExist(widget.snapshot.id).then((value) {
+      setState(() {
+        doesExists = value;
+      });
+    });
     if (widget.role == "owner") {
       return Scaffold(
         body: SingleChildScrollView(
           child: ConstrainedBox(
-            constraints: BoxConstraints(
-              minHeight: MediaQuery.of(context).size.height,
-            ),
-            child: widget.role == "owner"
-                ? Padding(
-                    padding: const EdgeInsets.only(top: 18.0),
-                    child: Center(
-                      child: Column(
-                        children: [
-                          CustomTitle(
-                            fontSize: 30,
-                            text: widget.snapshot['title'],
-                            color: Colors.black,
-                          ),
-                          Padding(
-                            padding: const EdgeInsets.all(8.0),
-                            child: Card(
-                                elevation: 10,
-                                child: Image.network(
-                                  widget.imageURL,
-                                  height:
-                                      MediaQuery.of(context).size.height / 1.25,
-                                )),
-                          ),
-                          Padding(
-                            padding: const EdgeInsets.only(right: 12.0),
-                            child: Row(
-                              mainAxisAlignment: MainAxisAlignment.end,
-                              children: [
-                                CustomTitle(
-                                  align: TextAlign.left,
-                                  fontSize: 20,
-                                  text: "By:",
-                                  color: Colors.black,
-                                ),
-                                CustomTitle(
-                                  align: TextAlign.right,
-                                  fontSize: 20,
-                                  text: widget.snapshot['twitter'],
-                                  color: Colors.black,
-                                ),
-                              ],
-                            ),
-                          ),
-                        ],
-                      ),
-                    ),
-                  )
-                : Padding(
-              padding: const EdgeInsets.only(top: 8),
-              child: Center(
-                child: Column(
-                  children: [
-                    CustomTitle(
-                      fontSize: 30,
-                      text: widget.snapshot['title'],
-                      color: Colors.black,
-                    ),
-                    Padding(
-                      padding: const EdgeInsets.all(8.0),
-                      child: Card(
-                          elevation: 10,
-                          child: Image.network(
-                            widget.imageURL,
-                            height:
-                            MediaQuery.of(context).size.height / 1.25,
-                          )),
-                    ),
-                  ],
-                ),
+              constraints: BoxConstraints(
+                minHeight: MediaQuery
+                    .of(context)
+                    .size
+                    .height,
               ),
-            ),
+              child: Padding(
+                padding: const EdgeInsets.only(top: 18.0),
+                child: Center(
+                  child: Column(
+                    children: [
+                      CustomTitle(
+                        fontSize: 30,
+                        text: widget.snapshot['title'],
+                        color: Colors.black,
+                      ),
+                      Padding(
+                        padding: const EdgeInsets.all(8.0),
+                        child: Card(
+                            elevation: 10,
+                            child: Image.network(
+                              widget.imageURL,
+                              height:
+                              MediaQuery
+                                  .of(context)
+                                  .size
+                                  .height / 1.25,
+                            )),
+                      ),
+                      Padding(
+                        padding: const EdgeInsets.only(right: 12.0),
+                        child: Row(
+                          mainAxisAlignment: MainAxisAlignment.end,
+                          children: [
+                            CustomTitle(
+                              align: TextAlign.left,
+                              fontSize: 20,
+                              text: "By:",
+                              color: Colors.black,
+                            ),
+                            CustomTitle(
+                              align: TextAlign.right,
+                              fontSize: 20,
+                              text: widget.snapshot['twitter'],
+                              color: Colors.black,
+                            ),
+                          ],
+                        ),
+                      ),
+                    ],
+                  ),
+                ),
+              )
           ),
         ),
       );
@@ -111,28 +110,45 @@ class _ImageViewState extends State<ImageView> {
                 // borderRadius: BorderRadius.circular(15)),
                 elevation: 10,
                 child: Container(
-                  height: MediaQuery.of(context).size.height / 1.15,
+                  height: MediaQuery
+                      .of(context)
+                      .size
+                      .height / 1.15,
                   child: Image.network(
                     widget.imageURL,
                     fit: BoxFit.fill,
                   ),
                 ),
               ),
-              widget.role == "curator"?
+              widget.role != "curator"
+                  ? //TODO CHANGE TO == Later
               IconButton(
-                  onPressed: (){
-                    FirebaseFirestore.instance.collection('selectedArt').doc(widget.snapshot.id).set({
-                      'title': widget.snapshot["title"],
-                      'id': widget.snapshot["id"],
-                      'description': widget.snapshot["description"],
-                      'twitter':widget.snapshot["twitter"],
-                      'images': widget.snapshot["images"],
-                      "userid":widget.snapshot["userid"],
-                      "type": widget.snapshot["type"],
-                      "imageThumbnail": widget.snapshot["imageThumbnail"],
-                    });
-                  }
-                  , icon: Icon(Icons.heart_broken_rounded))
+                  onPressed: () {
+                    if (doesExists) {
+                      print("tru");
+                      FirebaseFirestore
+                          .instance.collection("selectedArt").doc(widget.snapshot.id).delete();
+                    }
+                    else {
+                      print("false");
+                      FirebaseFirestore.instance
+                          .collection('selectedArt')
+                          .doc(widget.snapshot.id)
+                          .set({
+                        'title': widget.snapshot["title"],
+                        'id': widget.snapshot["id"],
+                        'description': widget.snapshot["description"],
+                        'twitter': widget.snapshot["twitter"],
+                        'images': widget.snapshot["images"],
+                        "userid": widget.snapshot["userid"],
+                        "type": widget.snapshot["type"],
+                        "imageThumbnail": widget.snapshot["imageThumbnail"],
+                      });
+                    }
+                  },
+                  icon: doesExists
+                      ? Icon(Icons.heart_broken_rounded)
+                      : Icon(Icons.print))
                   : SizedBox.shrink(),
             ],
           ),
